a7c570a7b66f1431a10e47a027983c65dd5a1565
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-13 21:23:46 +0500

    08

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
-//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-const AdditionResult= 2+6;
-console.log("Addition Result=",AdditionResult);
-const SubtractionResult= 3-5;
-console.log("Subtraction Result=",SubtractionResult);
-const multiplicationResult=6*6;
-console.log("Multiplication Result=",multiplicationResult);
-const divisionResult = 20/4;
-console.log("Division Result=",divisionResult);
+//You should create four lines that look like this:
+
+//console.log(5 + 3)
+
+//Your output should simply be four lines with the number 8 appearing once on each line.
+
+
+console.log("+".repeat(80));
+console.log(5 + 3);
+console.log("-".repeat(80));
+console.log(10 - 2);
+console.log("*".repeat(80));
+console.log(4 * 2);
+console.log("/".repeat(80));
+console.log(16 / 2);
