b4d71a7521edd9cb298988caf09110e773ad32a9
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-13 21:35:50 +0500

    9

--- index.ts
+++ index.ts
@@ -1,15 +1,5 @@
-//You should create four lines that look like this:
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 
-//console.log(5 + 3)
-
-//Your output should simply be four lines with the number 8 appearing once on each line.
-
-
-console.log("+".repeat(80));
-console.log(5 + 3);
-console.log("-".repeat(80));
-console.log(10 - 2);
-console.log("*".repeat(80));
-console.log(4 * 2);
-console.log("/".repeat(80));
-console.log(16 / 2);
+let favouritename:number=244401711
+let message ="My Favourite Number is"
+console.log(message,favouritename);
