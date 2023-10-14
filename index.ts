fe4fe55c19c45d609185d2a50f88f1e154c6982a
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-14 23:28:29 +0500

    16

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
-//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+• Add one new guest to the beginning of your array.
+
+• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.*/
 
 let GuestList:Array<string>=[
     "Sultan Muhammad Ali",
@@ -6,21 +11,23 @@ let GuestList:Array<string>=[
     "Sultan Bhudhar Aziz",
     "Mian Sarwar Bodla"
     ];
-    
+    for (let guest of GuestList){
 
-    let guesthowcannotcoming:string="Mian Sarwar Bodla"
-    console.log(`${guesthowcannotcoming} not coming`);
-
-    let newguest:string="Allama-e-Iqbal"
-    let indexofguesthowcannotcoming:number=GuestList.indexOf(guesthowcannotcoming)
-
-    if (indexofguesthowcannotcoming !== -1){
-
-        GuestList[indexofguesthowcannotcoming] = newguest
+        console.log(`Hello, ${guest} we just found a bigger dinner table`)
     }
+let newgueststart:string="Allama-e-Iqbal"
+GuestList.unshift(newgueststart);
 
-    GuestList.forEach((guestname:string) =>{
-    
-        console.log(`Dear ${guestname} you are invited in dinner, please join us`)
-        
-        });
+let newguestmiddle:string="Quaid-e-Azam"
+GuestList.splice(3,0,newguestmiddle); 
+console.log(GuestList);
+
+let guestatend:string="Zia Khan"
+GuestList.push(guestatend);
+
+console.log("New Invitation")
+
+for (let guest of GuestList){
+
+    console.log(`Hello, ${guest} you are invited at dinner`)
+};
