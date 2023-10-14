62676d3d7e60465b459f04f1e21ca799472e4cfa
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-14 19:37:28 +0500

    15

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
-//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
 
 let GuestList:Array<string>=[
-"Sultan Muhammad Ali",
-"Sultan Ahmad Ali",
-"Sultan Bhudhar Aziz",
-"Mian Sarwar Bodla"
-];
-GuestList.forEach((guestname) =>{
+    "Sultan Muhammad Ali",
+    "Sultan Ahmad Ali",
+    "Sultan Bhudhar Aziz",
+    "Mian Sarwar Bodla"
+    ];
+    
 
-console.log(`Dear ${guestname} you are invited in dinner, please join us`)
+    let guesthowcannotcoming:string="Mian Sarwar Bodla"
+    console.log(`${guesthowcannotcoming} not coming`);
 
-});
+    let newguest:string="Allama-e-Iqbal"
+    let indexofguesthowcannotcoming:number=GuestList.indexOf(guesthowcannotcoming)
+
+    if (indexofguesthowcannotcoming !== -1){
+
+        GuestList[indexofguesthowcannotcoming] = newguest
+    }
+
+    GuestList.forEach((guestname:string) =>{
+    
+        console.log(`Dear ${guestname} you are invited in dinner, please join us`)
+        
+        });
