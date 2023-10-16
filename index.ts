2bfac883fd26553e2eded3d94f6578c3f35683d6
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-17 01:49:54 +0500

    17

--- index.ts
+++ index.ts
@@ -1,33 +1,25 @@
-/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
-• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
-
-• Add one new guest to the beginning of your array.
-
-• Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.*/
-
-let GuestList:Array<string>=[
+//Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+let guestList = [
     "Sultan Muhammad Ali",
     "Sultan Ahmad Ali",
     "Sultan Bhudhar Aziz",
-    "Mian Sarwar Bodla"
-    ];
-    for (let guest of GuestList){
-
-        console.log(`Hello, ${guest} we just found a bigger dinner table`)
-    }
-let newgueststart:string="Allama-e-Iqbal"
-GuestList.unshift(newgueststart);
-
-let newguestmiddle:string="Quaid-e-Azam"
-GuestList.splice(3,0,newguestmiddle); 
-console.log(GuestList);
-
-let guestatend:string="Zia Khan"
-GuestList.push(guestatend);
-
-console.log("New Invitation")
-
-for (let guest of GuestList){
-
-    console.log(`Hello, ${guest} you are invited at dinner`)
-};
+    "Mian Sarwar Bodla",
+];
+// message about invitng only two people.
+console.log(`I can invite only two people for dinner.`);
+// 2nd step remove guest from th list untill only two name rmain.
+while (guestList.length > 2) {
+    let removeGuest = guestList.pop();
+    console.log(`Sorry, ${removeGuest}, I can't invite you to dinner.`);
+}
+// Part 3 - Print a message to each of the two people still on your list, letting them know they’re still invited.
+for (let guest of guestList) {
+    console.log(`Hello, ${guest}, you are still invited to dinner!`);
+}
+console.log(guestList);
+// 4th part: Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+// Removing last two
+guestList.pop();
+guestList.pop();
+// prnint empty string
+console.log(`Guest list after dinner:`, guestList);
