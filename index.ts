80ef2caefad6aa412d912e87320ae43db9d35744
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-29 19:32:19 +0500

    45

--- index.ts
+++ index.ts
@@ -1,19 +1,18 @@
+function createCar(manufacturer: string, model: string, ...properties: [string, any][]): any {
+    const car: any = {
+        manufacturer,
+        model,
+    };
 
-function makeSandwich(...items: string[]): void {
-    console.log("Sandwich Summary:");
-    if (items.length === 0) {
-        console.log("   - You ordered an empty sandwich. Please add some items.");
-    } else {
-        items.forEach((item, i) => {
-            console.log(`   ${i + 1}. ${item}`);
-        });
+    for (const [key, value] of properties) {
+        car[key] = value;
     }
-    console.log("\n");
+
+    return car;
 }
 
-// Call the function with different numbers of arguments
-makeSandwich()
-makeSandwich("Chiken brest", "Lettuce", "Tomato", "Mayonnaise");
-makeSandwich("Beef", "Cheese", "onion");
-makeSandwich("Peanut Butter", "Jelly", "Mayonnaise", "Honey");
-    
+// Example usage:
+const myCar: string = createCar("BMW", "Bi5", ["color", "red,yellow"], ["optional Feature", "sunroof"], ["speed", "320 KM/hour"]);
+
+
+console.log(myCar);
