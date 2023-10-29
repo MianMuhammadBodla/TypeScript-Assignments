0f2f38527f78debe8098aff35c9627e21ae1fc7e
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-29 19:01:24 +0500

    23

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
-//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
-
-const numbers = [1,2,3,4,5];
-console.log(numbers[10]);//undefined
-console.log(numbers[3]);//output is 4
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let car = 'subaru';
+console.log(car == 'subaru')//true
+console.log(car === 'subaru')//true
+console.log(car != 'subaru')//false
+console.log(car >= 'subaru')//true
+console.log(car <= 'subaru')//true
+console.log(car > 'subaru')//false
+console.log(car < 'subaru')//false
