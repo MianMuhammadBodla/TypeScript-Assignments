01267b03b686004b3676d6cfa48003dad7aefe54
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-29 19:30:04 +0500

    43

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // Array of magician's names
 const magicianNames: string[] = ["Habra Cadabra","Magic Mike","Amazing Alakazam","Amazing Alakazam","Magnificent Mystic"];
-function show_magicians(magicians: string[]): void {
+function showMagicians(magicians: string[]): void {
     for (const magician of magicians) {
         console.log(magician);
     }
@@ -14,6 +14,11 @@ function make_great(magicians: string[]): string[] {
 
 // Modify the array to add "the Great" to each magician's name
 const greatMagicianNames: string[] = make_great(magicianNames);
+
+// Call the function to show the original magician's names
+console.log("Original Magicians:");
+showMagicians(magicianNames);
+
 // Call the function to show the great magician's names
 console.log("\nGreat Magicians:");
-show_magicians(greatMagicianNames);
+showMagicians(greatMagicianNames);
