d902ad8f6435285a8f52b63214217744690e60a3
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-29 19:11:30 +0500

    30

--- index.ts
+++ index.ts
@@ -1,25 +1,8 @@
-/*Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
-• Make a array of your three favorite fruits and call it favorite_fruits.
-
-• Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas*/
-const favorite_fruits = ['apples', 'bananas', 'oranges'];
-
-if (favorite_fruits.includes('apples')) {
-  console.log('You really like apples!');
-}
-
-if (favorite_fruits.includes('bananas')) {
-  console.log('You really like bananas!');
-}
-
-if (favorite_fruits.includes('oranges')) {
-  console.log('You really like oranges!');
-}
-
-if (favorite_fruits.includes('pears')) {
-  console.log('You really like pears!');
-}
-
-if (favorite_fruits.includes('grapes')) {
-  console.log('You really like grapes!');
+const username = ['ahtasham','Abtahi','Tabish']
+for(const username of 'username');
+if(username){
+    console.log('Hello admin, would you like to see a status report?')
+}else
+{
+    console.log('Hello Eric, thank you for logging in again')
 }
