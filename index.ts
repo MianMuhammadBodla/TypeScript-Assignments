6b1bafefcd034fb681a25deb4339f5a7b812175f
MianMuhammadBodla <147194370+MianMuhammadBodla@users.noreply.github.com>
2023-10-29 18:53:32 +0500

    19

--- index.ts
+++ index.ts
@@ -1,25 +1,15 @@
-//Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
-let guestList = [
-    "Sultan Muhammad Ali",
-    "Sultan Ahmad Ali",
-    "Sultan Bhudhar Aziz",
-    "Mian Sarwar Bodla",
-];
-// message about invitng only two people.
-console.log(`I can invite only two people for dinner.`);
-// 2nd step remove guest from th list untill only two name rmain.
-while (guestList.length > 2) {
-    let removeGuest = guestList.pop();
-    console.log(`Sorry, ${removeGuest}, I can't invite you to dinner.`);
-}
-// Part 3 - Print a message to each of the two people still on your list, letting them know they’re still invited.
-for (let guest of guestList) {
-    console.log(`Hello, ${guest}, you are still invited to dinner!`);
-}
-console.log(guestList);
-// 4th part: Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
-// Removing last two
-guestList.pop();
-guestList.pop();
-// prnint empty string
-console.log(`Guest list after dinner:`, guestList);
+//Exercise no 19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+function inviteGuests(numGuests:String){
+    console.log(`We are inviting you to dinner with ${numGuests} guests.`);
+  
+  }
+  
+  
+  inviteGuests("Ali Malik.We are inviting you to dinner ");
+  inviteGuests("Ali Salman.We are inviting you to dinner ");
+  inviteGuests("Awais Khokhar.We are inviting you to dinner ");
+  inviteGuests("Arslan.We are inviting you to dinner ");
+  inviteGuests("Diyan.We are inviting you to dinner ");
+  inviteGuests("Abdullah.We are inviting you to dinner ");
